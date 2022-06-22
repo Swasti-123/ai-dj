b53e5ab3e121c3a5a57049442fd796976cfa4ed4
Swasti-123 <86404753+Swasti-123@users.noreply.github.com>
2022-06-22 18:31:28 +1000

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 sound=""
 
 function setup(){
-    canvas= canvasCreate(600, 500);
+    canvas= createCanvas(600, 500);
     canvas.center();
 
     video= createCapture(VIDEO);
@@ -16,14 +16,62 @@ function modelLoaded(){
 }
 function draw(){
     image(video, 0,0, 600,500);
+    
+    fill("red");
+    stroke("red");
+
+    if(scoreRightWrist>0.2){
+        circle(rightWristX,rightWristY, 20);
+
+        if(rightWristY > 0 && rightWristY <= 100){
+            document.getElementById("speed").innerHTML= "Speed= 0.5x";
+            sound.rate(0.5);
+        }
+        else if(rightWristY > 100 && rightWristY <= 200){
+            document.getElementById("speed").innerHTML= "Speed= 1x";
+            sound.rate(1);
+        }
+        else if(rightWristY > 200 && rightWristY <= 300){
+            document.getElementById("speed").innerHTML= "Speed= 1.5x";
+            sound.rate(1.5);
+        }
+        else if(rightWristY > 300 && rightWristY <= 400){
+            document.getElementById("speed").innerHTML= "Speed= 2x";
+            sound.rate(2);
+        }
+        else if(rightWristY > 400){
+            document.getElementById("speed").innerHTML= "Speed= 2.5x";
+            sound.rate(2.5);
+        }
+    }
+
+    if(scoreLeftWrist>0.2){
+        circle(leftWristX,leftWristY, 20);
+        numberLeftWristY= Number(leftWristY);
+        remove_decimals= floor(numberLeftWristY);
+        volume= remove_decimals/500;
+        sound.setVolume(volume);
+        document.getElementById("volume").innerHTML= "Volume= " + volume;
+    }
 }
 
 function preload(){
     sound=loadSound("music.mp3")
 }
 
+scoreRightWrist=0;
+scoreLeftWrist=0;
+
+leftWristX=0;
+leftWristY=0;
+
+rightWristX=0;
+rightWristY=0;
+
 function play(){
-    song.play();
+    sound.play();
+    sound.volume(1);
+    sound.rate(1);
 }
 
 function gotPoses(results){
@@ -31,6 +79,13 @@ function gotPoses(results){
         console.log(results);
         scoreRightWrist = results[0].pose.keypoints[10].score;
         scoreLeftWrist = results[0].pose.keypoints[9].score;
-        console.log("scoreRightWrist")
+        console.log("scoreRightWrist = " + scoreRightWrist + "scoreLeftWrist = " + scoreLeftWrist );
+
+        leftWristX=results[0].pose.leftWrist.x;
+        leftWristY=results[0].pose.leftWrist.y;
+
+        rightWristX=results[0].pose.rightWrist.x;
+        rightWristY=results[0].pose.rightWrist.y;
+
     }
 }
